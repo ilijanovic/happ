c03e7eec0cf3be5050b5830a515cfb55072a14ab
ilijanovic <ilija.marijanovic@energie.family>
2023-12-17 21:05:54 +0100

    m

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(26, GPIO.OUT);
-GPIO.setup(19, GPIO.OUT);
+
 
 
 
@@ -13,12 +12,16 @@ app = Flask(__name__)
 
 @app.route("/on")
 def hello_world():
+    GPIO.setup(26, GPIO.OUT);
+    GPIO.setup(19, GPIO.OUT);
     GPIO.output(26, True);
     GPIO.output(19, True); 
     return "work";
 
 @app.route("/off")
 def hell():
+    GPIO.setup(26, GPIO.OUT);
+    GPIO.setup(19, GPIO.OUT);
     GPIO.output(26, False);
     GPIO.output(19, False); 
     return "work";
