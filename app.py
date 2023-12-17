57e0f2242798f478ea75860e66c275d4b2e95d50
ilijanovic <ilija.marijanovic@energie.family>
2023-12-17 21:02:38 +0100

    m

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ app = Flask(__name__)
 
 @app.route("/on")
 def hello_world():
-    GPIO.output(26, GPIO.HIGH);
-    GPIO.output(19, GPIO.HIGH); 
-    return "work"
+    GPIO.output(26, True);
+    GPIO.output(19, True); 
+    return "work";
 
 @app.route("/off")
 def hell():
-    GPIO.output(26, GPIO.LOW);
-    GPIO.output(19, GPIO.LOW); 
-    return "work"
+    GPIO.output(26, False);
+    GPIO.output(19, False); 
+    return "work";
